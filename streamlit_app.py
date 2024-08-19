de61fe1fe0af9e37fdee3450f93b7e63504fdcca
ianbohlmann <ian.bohlmann@siemens-energy.com>
2024-08-19 13:20:54 +0200

    Update streamlit_app.py
    Fehler beheben

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -9,20 +10,21 @@ st.write(
 )
 
 
+
+name_on_order = st.text_input("Name on Smoothie")
+st.write("The name on your Smoothie will be:", name_on_order)
+
 cnx=st.connection("snowflake")
 session=cnx.session()
-
-
 my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
+    , 
 )
 if ingredients_list:
-    
-
     ingredients_string=''
 
     for fruit_chosen in ingredients_list:
@@ -30,10 +32,10 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
-    #st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
     time_to_insert=st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
